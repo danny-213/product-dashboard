a7f8931975dd4dacabecc603cd31be637a788652
Tran Huu Thanh <qtranhu@apac.corpdir.net>
2024-03-17 09:38:12 +0700

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,44 @@
 from flask import request, jsonify
-from config import app,db
-from models import Tire
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
+# SET UP FLASK
+app = Flask(__name__)
+CORS(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# DEFINE DATABASE
+class Tire(db.Model):
+    id = db.Column(db.String(80), primary_key=True)
+    full_desc = db.Column(db.String(120), unique=False, nullable=False)
+    full_dim = db.Column(db.String(120), unique=False, nullable=False)
+    rrp = db.Column(db.Integer, unique=False, nullable=False)
+    moMarking = db.Column(db.String(20), unique=False)
+    runflat = db.Column(db.String(20), unique=False)
+    imgUrl = db.Column(db.String(120), unique=False)
+    logo = db.Column(db.String(120), unique=False)
+    brand = db.Column(db.String(120), unique=False)
+
+    def to_json(self):
+        return {
+            "id":self.id,
+            "fullDesc":self.full_desc,
+            "fullDim":self.full_dim,
+            "rrp":self.rrp,
+            "moMarking":self.moMarking,
+            "runflat":self.runflat,
+            "imgUrl":self.imgUrl,
+            "logo":self.logo,
+            "brand":self.brand
+        }
+    
+# SET UP ROUTES FOR API CALL
 @app.route("/products", methods = ["GET"])
 
 def get_products():
